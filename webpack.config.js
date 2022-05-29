10c88e769f26921fc056de102a4798c11ea47406
kodai2610 <koudaisports26@gmail.com>
2022-05-29 13:34:34 +0900

    Extract CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -12,7 +14,7 @@ module.exports = {
 				test: /\.css/,//正規表現 .cssというファイル名を検知
 				use: [
 					{
-						loader: 'style-loader', //loaderは下から上に適用
+						loader: MiniCssExtractPlugin.loader, //loaderは下から上に適用
 					},
 					{
 						loader: 'css-loader', // css-loaderを使う
@@ -21,4 +23,10 @@ module.exports = {
 			},
 		],
 	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
 };
